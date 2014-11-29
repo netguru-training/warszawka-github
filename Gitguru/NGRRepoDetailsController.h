286a62781021eb8c3ca0e27ea93449af3bdc6b22
Adam Garstka <adam.garstka@netguru.co>
2014-11-29 13:22:09 +0100

    Repo details controller and view

---
+++ Gitguru/NGRRepoDetailsController.h
@@ -0,0 +1,13 @@
+//
+//  NGRRepoDetailsController.h
+//  Gitguru
+//
+//  Created by Adam Garstka on 29/11/14.
+//  Copyright (c) 2014 Netguru Sp. z o.o. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NGRRepoDetailsController : UIViewController
+
+@end
---
+++ Gitguru/NGRRepoDetailsController.m
@@ -0,0 +1,37 @@
+//
+//  NGRRepoDetailsController.m
+//  Gitguru
+//
+//  Created by Adam Garstka on 29/11/14.
+//  Copyright (c) 2014 Netguru Sp. z o.o. All rights reserved.
+//
+
+#import "NGRRepoDetailsController.h"
+
+@interface NGRRepoDetailsController ()
+
+@end
+
+@implementation NGRRepoDetailsController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
