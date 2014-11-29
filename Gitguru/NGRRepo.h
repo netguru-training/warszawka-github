c91740d1e6f864722b3184c2d47f17cfb643e671
Adam Garstka <adam.garstka@netguru.co>
2014-11-29 11:39:03 +0100

    Repo model

---
+++ Gitguru/NGRRepo.h
@@ -0,0 +1,17 @@
+//
+//  NGRRepo.h
+//  Gitguru
+//
+//  Created by Adam Garstka on 29/11/14.
+//  Copyright (c) 2014 Netguru Sp. z o.o. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NGRRepo : NSObject
+
+@property NSString *owner;
+@property NSURL *htmlURL;
+@property NSString *name;
+
+@end
---
+++ Gitguru/NGRRepo.m
@@ -0,0 +1,13 @@
+//
+//  NGRRepo.m
+//  Gitguru
+//
+//  Created by Adam Garstka on 29/11/14.
+//  Copyright (c) 2014 Netguru Sp. z o.o. All rights reserved.
+//
+
+#import "NGRRepo.h"
+
+@implementation NGRRepo
+
+@end
