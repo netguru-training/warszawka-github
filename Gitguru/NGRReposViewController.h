147901ffcd2b5cec1e8c0d15266b07e7312a2651
Adam Garstka <adam.garstka@netguru.co>
2014-11-29 11:54:20 +0100

    Repo cells

--- Gitguru/NGRReposViewController.h
+++ Gitguru/NGRReposViewController.h
@@ -2,7 +2,7 @@
 //  NGRReposViewController.h
 //  Gitguru
 //
-//  Created by Adrian Kashivskyy on 29.11.2014.
+//  Created by Adam Garstka on 29/11/14.
 //  Copyright (c) 2014 Netguru Sp. z o.o. All rights reserved.
 //
 
