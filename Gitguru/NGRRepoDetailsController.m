019c1637add36ff1dbc837b4a24df1c536a63c24
Tomasz Pewiński <pewniak747@gmail.com>
2014-11-29 17:09:01 +0100

    display repo properties

--- Gitguru/NGRRepoDetailsController.m
+++ Gitguru/NGRRepoDetailsController.m
@@ -9,8 +9,10 @@
 #import "NGRRepoDetailsController.h"
 
 @interface NGRRepoDetailsController ()
+@property (weak, nonatomic) IBOutlet UITableView *propertiesTableView;
 @property (weak, nonatomic) IBOutlet UILabel *titleLabel;
 @property (weak, nonatomic) IBOutlet UIImageView *avatarImageView;
+@property (strong, nonatomic) NSDictionary *properties;
 @end
 
 @implementation NGRRepoDetailsController
@@ -26,14 +28,24 @@
             weak_self.avatarImageView.image = image;
         }
     }];
+    
+    self.properties = @{
+                        @"Language": ((id)(self.repo.language) != [NSNull null]) ? self.repo.language : @"Unknown",
+                        @"Stars": [ NSString stringWithFormat:@"%lu", self.repo.starsCount ],
+                        @"Forks": [ NSString stringWithFormat:@"%lu", self.repo.forksCount ]
+                        };
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return 0;
+    return self.properties.count;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"ParamCell" forIndexPath:indexPath];
+    NSString *label = self.properties.allKeys[indexPath.row];
+    NSString *detail = self.properties[label];
+    cell.textLabel.text = label;
+    cell.detailTextLabel.text = detail;
     return cell;
 }
 
