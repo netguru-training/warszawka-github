22f623367a68624dd7792c3a8ce38239c87f2f0a
Adam Garstka <adam.garstka@netguru.co>
2014-11-30 14:54:49 +0100

    Users view controller

---
+++ Gitguru/NGRUsersViewController.h
@@ -0,0 +1,15 @@
+//
+//  NGRUsersViewController.h
+//  Gitguru
+//
+//  Created by Adam Garstka on 30/11/14.
+//  Copyright (c) 2014 Netguru Sp. z o.o. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NGRUsersViewController : UITableViewController
+
+@property NSMutableArray *users;
+
+@end
---
+++ Gitguru/NGRUsersViewController.m
@@ -0,0 +1,60 @@
+//
+//  NGRUsersViewController.m
+//  Gitguru
+//
+//  Created by Adam Garstka on 30/11/14.
+//  Copyright (c) 2014 Netguru Sp. z o.o. All rights reserved.
+//
+
+#import "NGRUsersViewController.h"
+#import "NGRUser.h"
+
+@interface NGRUsersViewController ()
+
+@end
+
+@implementation NGRUsersViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+    
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return [self.users count];
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"UserCell" forIndexPath:indexPath];
+    NGRUser *user = self.users[indexPath.row];
+    cell.textLabel.text = user.name;
+    cell.detailTextLabel.text = user.email;
+    
+    return cell;
+}
+
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+
+@end
